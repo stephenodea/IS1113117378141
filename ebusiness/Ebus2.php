73cafc0fd9420d893b32ace3234252245c3bab9d
Stephen O'Dea <117378141@umail.ucc.ie>
2018-02-11 16:59:10 +0000

    My initial Commit

---
+++ ebusiness/Ebus2.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <title> Enter Details</title>
+        
+        
+        <!--jQuery-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+    
+    <body>
+        
+        <h4>Please enter your payment details.</h4>
+        
+            <br />
+            
+            <form method = "POST" action = "Ebus3.php">
+                
+                <label for="user_pin">
+                     PIN 
+                </label>
+                
+                <input type="password" id="user_pin" placeholder="Card Pin" maxlength="4">
+                    
+                <button type="Submit" id="btnPurchase" disabled> 
+                    Proceed with Purchase 
+                </button>
+                
+            </form>
+            
+            <br />
+            
+            <button onClick="validateDetails()"> Validate </button>
+        
+        <script type="text/javascript" src="ebus2_validator.js"></script>
+        
+        
+    </body>
+    
+    
+    
+</html>
